e56145dc56b1584ba9e78b45b6e112259c4e2a97
Augusto94-coder <augustodileo94@gmail.com>
2025-08-29 08:54:17 +0200

    ciclo for

--- conta_numeri.js
+++ conta_numeri.js
@@ -5,9 +5,20 @@ Svolgimento:
    - Se n % 3 === 0 → significa che n è divisibile per 3.
    - Se n % 5 === 0 → significa che n è divisibile per 5.
 3. Le condizioni vanno controllate in ordine:
-   - Prima se è divisibile sia per 3 che per 5 (la meno stringente, cioè n % 3 === 0 && n % 5 === 0) → stampiamo "FizzBuzz".
-   - Poi se è divisibile solo per 3 → stampiamo "Fizz".
-   - Poi se è divisibile solo per 5 → stampiamo "Buzz".
-   - Altrimenti stampiamo il numero stesso.
+   - Prima se è divisibile sia per 3 che per 5 (la meno stringente, cioè n % 3 === 0 && n % 5 === 0) → stampa "FizzBuzz".
+   - Poi se è divisibile solo per 3 → stampa "Fizz".
+   - Poi se è divisibile solo per 5 → stampa "Buzz".
+   - Altrimenti stampa il numero stesso.
 */
 
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
